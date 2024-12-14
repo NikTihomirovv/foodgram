1d5a4d70762334ff323cecd94dce1f13d92e9165
Nikita Tihomirov <NikTihomrovv@yandex.ru>
2024-12-14 06:24:29 +0300

    other computer

--- backend/api_v1/permissions.py
+++ backend/api_v1/permissions.py
@@ -16,7 +16,7 @@ class IsAdminAuthorOrReadOnly(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return (
-            request.method == ('GET',)
+            request.method in SAFE_METHODS
             or obj.author == request.user
             or request.user.role == UserRole.ADMIN
         )
