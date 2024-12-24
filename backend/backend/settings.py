fa80a70f900e4f3da6ff75dc260450f2d655b1f5
Nikita Tihomirov <NikTihomrovv@yandex.ru>
2024-12-25 00:17:25 +0300

    pravki_3

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,11 +1,17 @@
 import os
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-SECRET_KEY = 'django-insecure-o$m=ujy+tjt9cg+wfnrjnk^s0-!k+^4r_pe)7__ikyd6eail4q'
-DEBUG = True
+from django.core.management.utils import get_random_secret_key
+from dotenv import load_dotenv
 
-ALLOWED_HOSTS = ['localhost', '8080', '127.0.0.1', '89.169.160.21', 'foodgramka.ydns.eu']
+
+load_dotenv()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
+DEBUG = str(os.getenv('DEBUG', False)).lower() == 'true'
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(', ')
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'collected_static'
@@ -66,23 +72,23 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'backend.wsgi.application'
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'django'),
-        'USER': os.getenv('POSTGRES_USER', 'django'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', ''),
-        'PORT': os.getenv('DB_PORT', 5432)
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('POSTGRES_DB', 'django'),
+#         'USER': os.getenv('POSTGRES_USER', 'django'),
+#         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+#         'HOST': os.getenv('DB_HOST', ''),
+#         'PORT': os.getenv('DB_PORT', 5432)
+#     }
+# }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
