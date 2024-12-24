b5cc60d4e42ed1dd026596c1e56ae7d94b4c48f8
Nikita Tihomirov <NikTihomrovv@yandex.ru>
2024-12-25 00:37:15 +0300

    pravki_3

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -5,7 +5,7 @@ from django.core.management.utils import get_random_secret_key
 from dotenv import load_dotenv
 
 load_dotenv()
-
+load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
